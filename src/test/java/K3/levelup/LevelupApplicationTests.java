d0ce76703ee1b34c7bdd11614336ab6fc9a71b81
kangsan <piopiop1178@gmail.com>
2021-07-13 18:32:34 +0900

    spring initialization

---
+++ src/test/java/K3/levelup/LevelupApplicationTests.java
@@ -0,0 +1,13 @@
+package K3.levelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
