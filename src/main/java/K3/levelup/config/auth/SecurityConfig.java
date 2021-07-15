b0cf8b74bc7935b75dcbfe2caa5d41b92aa9f280
kangsan <piopiop1178@gmail.com>
2021-07-15 15:33:08 +0900

    add loginhandler(for setting redirect page after login)

--- src/main/java/K3/levelup/config/auth/SecurityConfig.java
+++ src/main/java/K3/levelup/config/auth/SecurityConfig.java
@@ -1,5 +1,6 @@
 package K3.levelup.config.auth;
 
+import K3.levelup.config.auth.dto.CustomLoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,14 +28,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //*** csrf, header frame 설정 관련
                 .and()
                     .authorizeRequests()//URL별 권한 관리 설정 옵션의 시작점
-                    .antMatchers("/", "/css/**", "images/**",
-                            "/js/**", "h2-console/**").permitAll()
+                    .antMatchers("/",
+                            "/error",
+                            "/favicon.ico",
+                            "/**/*.png",
+                            "/**/*.gif",
+                            "/**/*.jpg",
+                            "/**/*.svg",
+                            "/**/*.html",
+                            "/**/*.css",
+                            "/**/*.js",
+                            "h2-console/**").permitAll()
                     .anyRequest().authenticated() //위에 설정한 url 제외 모두 로그인한 사용자에게만 허용
                 .and()
                     .logout()
                         .logoutSuccessUrl("/") //로그아웃 성공시 redirect
                 .and()
                     .oauth2Login()//OAuth2 로그인 기능 설정 옵션 시작점
+                        .loginPage("/")//로그인 페이지 설정 -> 로그인 안한 상태로 권한 필요한 url 들어갈 때 로그인 페이지로 리다이렉트
+                        .successHandler(new CustomLoginSuccessHandler()) //로그인 성공 후 행위 설정 (리다이렉트 페이지 설정 위해)
                         .userInfoEndpoint()//OAuth2 로그인 성공 이후 사용자 정보 가져올 때 설정 담당
                             .userService(customOAuth2UserService); //소셜 로그인 성공 시 유저 정보 관련 사용할 UserService 구현체 등록
 
