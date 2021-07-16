36ac91058d9ef20ab85e0ba3a5baedb4496fbddc
kangsan <piopiop1178@gmail.com>
2021-07-16 17:47:28 +0900

    블로그 등록 컨트롤러 고민중

--- src/main/java/K3/levelup/controller/ProfileController.java
+++ src/main/java/K3/levelup/controller/ProfileController.java
@@ -5,15 +5,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/user") //user로 시작되는 url에 맵핑
 public class ProfileController {
     private final HttpSession httpSession;
 
-    @GetMapping("/user")
+    @GetMapping("")
     public String profile(Model model) {
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
@@ -22,4 +25,14 @@ public class ProfileController {
 
         return "userdetail";
     }
+
+    //블로그 등록
+    //url 어떻게 설정하는 게 좋을지?
+    //등록하고 userdetail 페이지로 redirect? 값만 변경할 수 있는지?
+//    @PostMapping("/blog")
+//    public String registerBlog(String url) {
+//        //폼 내부의 url 받고 blog 객체 생성
+//        // user 내부에 추가 -> user entity 변경 필요 - 블로그에서 유저를 참조하는게 맞는지?? 고민 필요
+//
+//    }
 }
